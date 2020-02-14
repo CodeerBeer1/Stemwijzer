f129b7e682603214240f5e5a8b3dee72cda0f427
Khizer Butt <99053182@mydavinci.nl>
2020-02-14 09:05:48 +0100

    .

--- StemwijzerJS.js
+++ StemwijzerJS.js
@@ -1,29 +1,67 @@
-var stemwijzerModal = document.getElementById("modal");
+var stemwijzerModal = document.getElementById("intro-modal");
+var loadingModal = document.getElementById("loading-modal");
+var secondaryModal = document.getElementById("secondary-modal");
+var stellingenModal = document.getElementById("stellingen-modal");
 var stemwijzerLogo = document.getElementById("stemwijzer-logo");
 var stemwijzerStartext = document.getElementById("starText");
 var stemwijzerStartbutton = document.getElementById("stemwijzer-startbutton");
 var loadingGif = document.getElementById("loading-icon");
+var stellingTitle = document.getElementById("stelling-title");
 var stellingText = document.getElementById("stelling-text");
+var disagreeButton = document.getElementById("disagree-button");
+var neitherButton = document.getElementById("neither-button");
+var agreeButton = document.getElementById("agree-button");
+var previousButton = document.getElementById("previous-button");
 
-var questionCount = 0;
+var choices = [];
+
+function back()
+{
+
+    if (choices.length == 0)
+    {
+        intro();
+    }
+    else
+    {
+        choices.pop();
+        questionSetup();
+    }
+    
+}
+
+function intro()
+{
+
+    document.getElementById("stellingen-modal").className = "hide";
+    document.getElementById("intro-modal").className = "grid";
+}
 
 function startStemwijzer()
 {
-    stemwijzerStartext.innerHTML = "Aan het laden..."
-    stemwijzerStartbutton.className = "hide";
-    loadingGif.className = "show";
-    setTimeout(function()
-    {
-        stemwijzerStartext.className = "hide"
-        loadingGif.className = "hide";
-        questionSetup();
-    }, 4000);
+    secondaryModal.className = "hide";
+    stemwijzerModal.className = "hide";
+    loadingModal.className = "grid";
+
+    setTimeout(function() {questionSetup()}, 2000);
+}
+
+function nextQuestion(value)
+{
+    choices.push(value);
+    questionSetup();
+    
 }
 
 function questionSetup()
 {
 
-    stellingText.className = "show";
-    stellingText.innerHTML = subjects[questionCount].statement;
+    loadingModal.className = "hide";
+    stellingenModal.className = "grid";
+
+    stellingTitle.innerHTML = subjects[choices.length].title;
+    stellingText.innerHTML = subjects[choices.length].statement;
 
 }
+
+intro();
